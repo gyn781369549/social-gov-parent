2248697e255814f91e04c148b3d059e58bbe01d9
yng <gyn781369549@live.com>
2016-09-04 09:36:38 +0800

    add

---
+++ social-common/src/main/java/com/coaware/social/common/chart/WebChartData.java
@@ -0,0 +1,30 @@
+package com.coaware.social.common.chart;
+
+
+
+public class WebChartData {
+
+	private String option;
+	private String msg;
+	private Boolean state;
+	public String getOption() {
+		return option;
+	}
+	public void setOption(String option) {
+		this.option = option;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public Boolean getState() {
+		return state;
+	}
+	public void setState(Boolean state) {
+		this.state = state;
+	}
+	 
+	 
+}
---
+++ social-common/src/main/java/com/coaware/social/common/chart/XYPlot.java
@@ -0,0 +1,32 @@
+package com.coaware.social.common.chart;
+
+/**
+ * 统计显示类的
+ * @author Administrator
+ *
+ */
+public class XYPlot {
+	
+		private Integer id;	
+		private String name;
+		private Integer total;
+		public Integer getId() {
+			return id;
+		}
+		public void setId(Integer id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public Integer getTotal() {
+			return total;
+		}
+		public void setTotal(Integer total) {
+			this.total = total;
+		}
+		
+}
